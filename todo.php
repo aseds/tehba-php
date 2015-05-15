06e91e219fb50a0fbb92ba513cb1f0d852a58967
aseds <s.sami.ghayeni@gmail.com>
2015-05-15 07:31:08 +0430

    Create todo.php

---
+++ todo.php
@@ -0,0 +1,23 @@
+<?php 
+//  done: uploading images
+    todo: simulating a home page with db content
+    todo: making 
+        database table 
+        &   
+        its fields
+          event_id
+          event_content(markdown)
+          event_visibility
+          event_type  // event_type is better than event_size cause the 'size' is confusing    
+          event_image   //it's a FKey it was events_image_id. i want to use event_id in here too! but thats wrong
+          ///// it seems i should re-create the table with collation of 'utf8_persian_ci' or 'utf8_general_ci'?
+      
+    todo: get content from db into home page
+    todo: admin page with upload an-d typing feature
+    todo: saving image ids in the database
+    todo: how put markdwon using php
+      
+    todo: drag-an-drop image uploading
+    todo: should the website have 'load more' feature?
+    todo: allow the user to> convert a big event to small event: the image should be able to be lefty oR righty
+    todo: how to regex in domains?    
