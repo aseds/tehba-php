c0e58bc1b525b6358d805c69d6c8799b21b29f9a
aseds <s.sami.ghayeni@gmail.com>
2015-05-15 07:33:36 +0430

    Create upload_form.php

---
+++ snippets/upload_form.php
@@ -0,0 +1,5 @@
+<form action="pages/upload.php" method="post" enctype="multipart/form-data">
+  Select image to upload:<br>
+  <input type="file" name="photo" id="photo"><br>
+  <input type="submit" value="Upload Image" name="submit">
+</form>
