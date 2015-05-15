6ef8cfffd3f39ecdeff822a32f57210f75a48c83
aseds <s.sami.ghayeni@gmail.com>
2015-05-15 07:30:44 +0430

    Create admin.php

---
+++ admin.php
@@ -0,0 +1,11 @@
+<!DOCTYPE>
+<html>
+  <head></head>
+  <body>
+    this is admin <br>
+    go to <a href="/">homepage</a> <br><br>
+
+    <?php 
+      include_once("snippets/upload_form.php");
+    ?>
+</body>
